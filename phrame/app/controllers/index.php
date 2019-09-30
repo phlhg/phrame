6ee76715f23eb2b40c1b82fee77fd6686e9abeac
phlhg <36929410+phlhg@users.noreply.github.com>
2019-09-30 22:12:59 +0200

    [REFAC] Remove form for testing upload

--- phrame/app/controllers/index.php
+++ phrame/app/controllers/index.php
@@ -7,19 +7,6 @@
         function Main(){
             $this->view("Index");
             $this->view->var->set("page_title","Welcome");
-            $this->view->var->set("output",json_encode(\Helper\Upload::files()));
-            if(\Helper\Upload::exists("test")){
-                $files = \Helper\Upload::get("test");
-                $output = "";
-                foreach($files as $file){
-                    $output .= "<br/><bR/>".$file->name;
-                    $output .= "<br/>".$file->ext()."<br/><br/>";
-                    $output .= htmlentities($file->content());
-                }
-                $this->view->var->set("output",$output);
-            } else {
-                $this->view->var->set("output","None");
-            }
         }
 
     }
--- phrame/app/views/index.php
+++ phrame/app/views/index.php
@@ -1,9 +1,4 @@
 <?php $_VIEW->add("phrame/section/header"); ?>
     <h1>Welcome to PHRAME<img src="/phramer/img/icons/frame.svg"/></h1>
     <p>PHRAME is a lighweight PHP-framework for creating websites</p>
-    <form action="" method="POST" enctype="multipart/form-data">
-        <input type="file" multiple name="test[]"/><br/><br/>
-        <input type="submit" value="Hochladen"/>
-    </form>
-    <pre><?=$_VAR->get("output")?></pre>
 <?php $_VIEW->add("phrame/section/footer"); ?>
