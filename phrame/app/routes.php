0f4b42f6cdfb2cc30fc9143fb24745c55eea1e6b
phlhg <36929410+phlhg@users.noreply.github.com>
2019-10-01 07:55:14 +0200

    [FEAT] Set url args to reload page

--- phrame/app/routes.php
+++ phrame/app/routes.php
@@ -1,7 +1,7 @@
 <?php
 
     $_ROUTER->set("/","Index");
-    $_ROUTER->set("/article/{id}/{name}/","Articles/Show")->where(["id" => '\d+', "name" => "[\w_]+"]);
+    $_ROUTER->set("/article/{id}/{name}/","Articles/Show")->where(["id" => '\d+', "name" => "[\w-]+"]);
     $_ROUTER->set("/article/","Articles/Index")
 
 ?>
