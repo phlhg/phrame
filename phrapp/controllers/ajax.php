d717c7dd6c78393e22ee03cce9dbe7b9958ed0bb
phlhg <36929410+phlhg@users.noreply.github.com>
2019-10-06 14:33:05 +0200

    [FEAT] Add most common output types

--- phrapp/controllers/ajax.php
+++ phrapp/controllers/ajax.php
@@ -5,9 +5,19 @@
     class Ajax extends \Controller {
 
         function Main(){
-            $this->view("Phrame/json");
+            $this->view("Phrame/Output/XML");
             $this->view->var->set("test","lol");
             $this->view->var->set("hello","world");
+            $this->view->var->set("settings",[
+                [
+                    "name" => "show_alert",
+                    "value" => true
+                ],
+                [
+                    "name" => "show_msg",
+                    "value" => false
+                ]
+            ]);
         }
 
     }
