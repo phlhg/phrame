dd6859aad6b4e4966819eb79b9712870b41ebed3
phlhg <36929410+phlhg@users.noreply.github.com>
2019-10-11 19:11:28 +0200

    Make App a submodule

--- phrapp/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $_ROUTER->GET("/","Index");
-    $_ROUTER->GET("/article/{id}/{name}/","Articles/Show")->where(["id" => '\d+', "name" => "[\w-]+"]);
-    $_ROUTER->GET("/article/","Articles/Index");
-
-    $_ROUTER->GET("/ajax/{type}/","Ajax")->where(["type" => "\w+"]);
-
-?>
--- phrapp/views/index.php
+++
@@ -1,4 +0,0 @@
-<?php $_VIEW->add("phrame/section/header"); ?>
-    <h1>Welcome to PHRAME<img src="/phramer/img/icons/frame.svg"/></h1>
-    <p>PHRAME is a lighweight PHP-framework for creating websites</p>
-<?php $_VIEW->add("phrame/section/footer"); ?>
