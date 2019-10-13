c36850729129a35eeac5739ccc44ee435f3cdba1
PHLHG <36929410+phlhg@users.noreply.github.com>
2019-10-13 23:21:33 +0200

    [REFAC] Change structure of conf

--- phrame/default/views/section/header.php
+++ phrame/default/views/section/header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-    <title><?=$_VAR->get("page_title","[title]").Conf::get("page/title/appendix")?></title>
+    <title><?=$_VAR->get("page_title","[title]").Conf::get("view/html/title/appendix")?></title>
     <meta charset="utf-8" />
     <link href="/phramer/css/main.css" rel="stylesheet" />
 </head>
