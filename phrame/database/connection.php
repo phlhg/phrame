52bef39877b7bcbdc07f87bbf403aabdce13122a
PHLHG <36929410+phlhg@users.noreply.github.com>
2019-10-14 12:15:00 +0200

    [REFAC] Add charset to connection

--- phrame/database/connection.php
+++ phrame/database/connection.php
@@ -8,13 +8,15 @@
         private $username = "";
         private $password = "";
         private $dbname = "";
+        private $charset = "utf8";
         private $connection;
 
-        public function __construct($host,$dbname,$credentials){
+        public function __construct($host,$dbname,$credentials,$charset="utf8"){
             $this->host = $host;
             $this->dbname = $dbname;
             $this->username = $credentials[0];
             $this->password = $credentials[1];
+            $this->charset = $charset;
             $this->connect();
         }
 
@@ -23,7 +25,7 @@
         }
 
         private function connect(){
-            $this->connection = new \PDO('mysql:host='.$this->host.';dbname='.$this->dbname,$this->username,$this->password);
+            $this->connection = new \PDO('mysql:host='.$this->host.';dbname='.$this->dbname.";charset=".$this->charset,$this->username,$this->password);
         }
 
         public static function add(...$args){
