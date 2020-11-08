d70eee41078dae1d8892b7a43d1cca2e170ee80b
Philippe Hugo <philippe.hugo@swissonline.ch>
2020-11-08 19:06:54 +0100

    [FIX] Update link to project

--- phrame/default/views/section/footer.php
+++ phrame/default/views/section/footer.php
@@ -1,3 +1,3 @@
-        <span class="meta">A site powered by <a href="//phlhg.ch/projects/phrame" target="_blank">phrame</a></span>
+        <span class="meta">A site powered by <a href="//phlhg.ch/projects/7/phrame-a-php-framework/" target="_blank">phrame</a></span>
     </div>
 </body>
